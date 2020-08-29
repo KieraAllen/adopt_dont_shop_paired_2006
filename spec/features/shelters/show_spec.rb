ff4acb9f75622bd07d057f11e662a3e90bef1c47
Kiera Allen <kieraaallen@gmail.com>
2020-08-29 09:35:26 -0600

    Add reviews to show page

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -1,14 +1,3 @@
-# User Story 3, Shelter Show
-#
-# As a visitor
-# When I visit '/shelters/:id'
-# Then I see the shelter with that id including the shelter's:
-# - name
-# - address
-# - city
-# - state
-# - zip
-
 require 'rails_helper'
 
 RSpec.describe 'shelters show page', type: :feature do
@@ -26,5 +15,49 @@ RSpec.describe 'shelters show page', type: :feature do
     expect(page).to have_text(shelter_1.city)
     expect(page).to have_text(shelter_1.state)
     expect(page).to have_text(shelter_1.zip)
+  end
+
+  it 'can see reviews for shelter' do
+    shelter_1 = Shelter.create(name: "The Dragon's Dream",
+                               address: '1554 Diamond Lane',
+                               city: 'Destin',
+                               state: 'FL',
+                               zip: '32540')
+    shelter_2 = Shelter.create(name: 'Bougie Beasts',
+                               address: '2244 Ruby Ave',
+                               city: 'Cedar Rapids',
+                               state: 'IA',
+                               zip: '52227')
+
+    review_1 = Review.create( image: 'https://www.aspca.org/sites/default/files/styles/latest_story_465x458/public/field/image/blog/blog_separation-anxiety_082720_thumb.jpg?itok=99rlR42z',
+                              title: 'Very cool!',
+                              content: 'I rather liked the place!',
+                              rating: '5',
+                              shelter_id: shelter_1.id)
+    review_2 = Review.create( title: 'Okay',
+                              content: 'It was not the worst',
+                              rating: '3',
+                              shelter_id: shelter_1.id)
+    review_3 = Review.create( title: 'Pretty bad',
+                              content: 'I do not think I will be back',
+                              rating: '1',
+                              shelter_id: shelter_2.id)
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_xpath("//img[@src='#{review_1.image}']")
+    expect(page).to have_text(review_1.title)
+    expect(page).to have_text(review_2.title)
+    expect(page).to_not have_text(review_3.title)
+
+    expect(page).to have_text(review_1.content)
+    expect(page).to have_text(review_2.content)
+    expect(page).to_not have_text(review_3.content)
+
+    expect(page).to have_text(review_1.rating)
+    expect(page).to have_text(review_2.rating)
+
+    expect(page).to have_text(shelter_1.name)
+    expect(page).to_not have_text(shelter_2.name)
   end
 end
