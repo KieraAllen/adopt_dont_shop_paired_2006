5d739f55959198702c77691ec3b2ea9c4fc9fb15
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-30 16:43:29 -0600

    Cleaning up Nav Bar.

--- spec/features/favorites/add_pet_spec.rb
+++ spec/features/favorites/add_pet_spec.rb
@@ -18,60 +18,40 @@ RSpec.describe "When a user adds pet to their favorites" do
 
     click_button "Add Pet To Favorites"
 
-
     expect(page).to have_content("#{pet_1.name} has been added to your Favorites!")
   end
 
-  it "the message correctly increments for multiple pets" do
-    shelter = Shelter.create(name: 'Denver Dumb Friends League')
-    pet_1 = shelter.pets.create(name: 'Eureka', approximate_age: 12, sex: 'F')
-    pet_2 = shelter.pets.create(name: "Tarqui", approximate_age: 2, sex: 'M')
+  it "displays the total number of pets in the favorites" do
+    shelter_1 = Shelter.create(name: "The Dragon's Dream",
+                               address: '1554 Diamond Lane',
+                               city: 'Destin',
+                               state: 'FL',
+                               zip: '32540')
+    pet_1 = Pet.create(image: 'TBD',
+                        name: 'Drax',
+                        approximate_age: '102',
+                        sex: 'M',
+                        shelter_id: shelter_1.id)
 
-    visit '/pets'
-
-    within("#pet-#{pet_1.id}") do
-      click_button "Add Pet To Favorites"
-    end
-
-    within("#pet-#{pet_2.id}") do
-      click_button "Add Pet To Favorites"
-    end
-
-    within("#pet-#{pet_1.id}") do
-      click_button "Add Pet To Favorites"
-    end
-
-    expect(page).to have_content("You now have #{pluralize(quantity, 'copy')} of #{pet.name} in your Favorites.")
-
-    expect(page).to have_content("Favorites: 3")
-  end
-
-  it "displays the total number of pets in the cart" do
-    shelter = Shelter.create(name: 'Denver Dumb Friends League')
-    pet_1 = shelter.pets.create(name: 'Eureka', approximate_age: 12, sex: 'F')
-    pet_2 = shelter.pets.create(name: "Tarqui", approximate_age: 2, sex: 'M')
-
-    visit "/pets"
+    visit "/pets/#{pet_1.id}"
 
     expect(page).to have_content("Favorites: 0")
 
-    within("#pet-#{pet_1.id}") do
-      click_button "Add Pet to Favorites"
-    end
+    click_button "Add Pet To Favorites"
 
     expect(page).to have_content("Favorites: 1")
 
-    within("#pet-#{pet_2.id}") do
-      click_button "Add Pet to Favorites"
-    end
+    expect(page).to_not have_content("Add Pet to Favorites")
 
-    expect(page).to have_content("Favorites: 2")
+    expect(page).to have_content("Remove Pet From Favorites")
 
-    within("#pet-#{pet_1.id}") do
-      click_button "Add Pet to Favorites"
-    end
+    click_button "Remove Pet From Favorites"
 
-    expect(page).to have_content("Favorites: 3")
+    expect(page).to have_content("Favorites: 0")
+
+    expect(page).to_not have_content("Remove Pet From Favorites")
+
+    expect(page).to have_content("Add Pet to Favorites")
   end
 
 end
